8c61161f420a85c1f265c322ac819b95f1d02737
mennamokhtar <mennamokhtarali@gmail.com>
2019-02-14 12:47:05 +0200

    thermonitor

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,28 +1,113 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-Button button;
+
+    EditText Email;
+    EditText Password;
+    EditText donthave;
+    Button login;
+    private FirebaseAuth auth;
+    Button register;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-        button=(Button)findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        FirebaseApp.initializeApp(this);
+        auth = FirebaseAuth.getInstance();
+        login = (Button) findViewById(R.id.button);
+        register = (Button) findViewById(R.id.register);
+        Email = (EditText) findViewById(R.id.mail);
+       // donthave = (EditText) findViewById(R.id.no);
+
+        Password = (EditText) findViewById(R.id.pass);
+//        login.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                openlist();
+//            }
+//        });
+        register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               openlist();
+                openlist1();
+            }
+        });
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = Email.getText().toString();
+                final String password = Password.getText().toString();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // progressBar.setVisibility(View.VISIBLE);
+
+                //authenticate user
+                auth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                // If sign in fails, display a message to the user. If sign in succeeds
+                                // the auth state listener will be notified and logic to handle the
+                                // signed in user can be handled in the listener.
+                                //progressBar.setVisibility(View.GONE);
+                                if (!task.isSuccessful()) {
+                                    // there was an error
+                                    if (password.length() < 6) {
+                                        Password.setError("Password's not correct");
+                                    } else {
+                                        Toast.makeText(MainActivity.this, "mail is not correct", Toast.LENGTH_LONG).show();
+                                    }
+                                } else {
+                                    Intent intent = new Intent(MainActivity.this, list.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                            }
+                        });
             }
         });
     }
-    public void openlist(){
-        Intent intent=new Intent(this,list.class);
 
-       startActivity(intent);
-     }
+
+    //    public void openlist(){
+//        Intent intent=new Intent(this,list.class);
+//
+//       startActivity(intent);
+//     }
+    public void openlist1() {
+        Intent intent = new Intent(this, register.class);
+
+        startActivity(intent);
+    }
+
+
 }
--- app/src/main/java/com/example/myapplication/list.java
+++ app/src/main/java/com/example/myapplication/list.java
@@ -4,19 +4,26 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class list extends AppCompatActivity {
     String item[]=new String[]{"android","iphone","windows","blackberry","linux"};
+    int[] images={R.drawable.gogo,R.drawable.iphone,R.drawable.zakarya,R.drawable.blackberry,R.drawable.linux};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
         ListView listview=(ListView)findViewById(R.id.list_item);
-        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,item);
-        listview.setAdapter(adapter);
+        CustomAdapter customAdapter=new CustomAdapter();
+       // ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,item);
+        listview.setAdapter(customAdapter);
         listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
@@ -24,5 +31,38 @@ public class list extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+// hakteb hna class esmo cutom adapter
+
+
+
+}
+
+class CustomAdapter extends BaseAdapter{
+
+    @Override
+    public int getCount() {
+        return images.length;
     }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        view=getLayoutInflater().inflate(R.layout.sowar,null);
+        ImageView imageView=(ImageView)view.findViewById(R.id.imageView);
+        TextView textView=(TextView)view.findViewById(R.id.textView);
+        imageView.setImageResource(images[i]);
+        textView.setText(item[i]);
+        return view;
+    }
+
+}
 }
